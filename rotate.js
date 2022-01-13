43d165f27f83b83dd8607e3dac84cee47ce6cebc
Bruno Marques <38708729+besmarques@users.noreply.github.com>
2022-01-13 09:58:21 +0000

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,6 +1,9 @@
 let matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
 
+//rotate clockwise 90 degrees
 let result = matrix[0].map((val, index) => matrix.map(row => row[index]).reverse());
+
+//rotate counter-clockwise 90 degrees
 let resultInverted = matrix[0].map((val, index) => matrix.map(row => row[index])).reverse();
 
 console.log(result);
